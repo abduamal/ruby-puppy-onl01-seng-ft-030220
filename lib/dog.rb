bce93e51f8731b1fa3d9a0e6aa9e44bf02e6cfd8
Amal Abdu <abduamal@ymail.com>
2020-03-15 22:32:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  @@all = []
   
+  def self.all
+    @@all
+  end
+  def save
+    @@all << self
+  end
+  def initialize()
+  
+  def print_all
+    puts "Your name is #{name}!"
+  end
 end
